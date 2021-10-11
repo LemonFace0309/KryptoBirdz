7a9f2c8fb4951a67eba487018d9da63ff66481d1
Charles Liu <charles.liu0309@gmail.com>
2021-10-11 13:05:06 -0400

    added minting function to ERC721.sol

--- src/contracts/ERC721.sol
+++ src/contracts/ERC721.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract ERC721 {
-
   /*
 
   building out the minting function
@@ -13,5 +11,22 @@ contract ERC721 {
     5. create an event that emits a transfer log - contract address, where it is being minted to, the id
 
   */
+
+contract ERC721 {
+  mapping(uint256 => address) private _tokenOwner;
+  mapping(address => uint256) private _ownedTokensCount;
+
+
+  function _exists(uint256 tokenId) internal view returns(bool) {
+    address owner = _tokenOwner[tokenId];
+    return owner != address(0);
+  }
+
+  function _mint(address to, uint256 tokenId) internal {
+    require(to != address(0), 'ERC721: minting to the zero address');
+    require(!_exists(tokenId), 'ERC721: token already minted');
+    _tokenOwner[tokenId] = to;
+    _ownedTokensCount[to] += 1;
+  } 
 
 }
